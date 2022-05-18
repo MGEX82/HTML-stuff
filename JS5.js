24477532bdd1576b84d134754f7840fdc875457c
Matt <mglion82@yahoo.co.uk>
2022-05-18 14:00:05 +0100

    Template literal stuff

---
+++ JS5.js
@@ -0,0 +1,9 @@
+let totalMoney = 4000;
+let moneyPaidSoFar = 2348;
+let totalLeftToPay = totalMoney - moneyPaidSoFar;
+
+let str1 = 'The total bill is';
+let str2 = 'the remaining amount of money to be paid is';
+let str3 = str1 + totalMoney + str2 + totalLeftToPay;
+
+console.log(str3);
