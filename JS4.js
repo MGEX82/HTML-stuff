8a6a01aad4f018dc95b16b3ecfa6e05038c7baed
Matt <mglion82@yahoo.co.uk>
2022-05-18 12:53:25 +0100

    Variable exercise 1

---
+++ JS4.js
@@ -0,0 +1,11 @@
+let a;
+let b = "12345";
+let c = 12344;
+let d = true;
+let e = {a: "Javascript"};
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
